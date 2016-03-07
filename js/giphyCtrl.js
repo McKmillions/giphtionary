f55630882544afb8a11e9d5fa8c4a1082a5b0751
McKmillions <zachmckimmins@gmail.com>
2016-03-07 15:59:05 -0600

    integrated giphy api

--- js/giphyCtrl.js
+++ js/giphyCtrl.js
@@ -1,6 +1,19 @@
 angular.module('giphTionary')
 .controller('giphyCtrl', function($scope, $stateParams, giphyService, userService){
 
+  // giphyService.getGiphData($stateParams.giphId)
+  // .then(function(result) {
+  //   $scope.giph = result.data;
+  // });
 
+  $scope.getGiphData = function() {
+    giphyService.getData($scope.search)
+    .then(function(data) {
+      console.log(data);
+      // $scope.giphData = data[0].images.fixed_height.url;
+      $scope.giphData = data.fixed_height_downsampled_url;
+      $scope.search = ""
+    });
+  };
 
 });
--- js/giphyService.js
+++ js/giphyService.js
@@ -1,5 +1,33 @@
-angular.module('giphTionary')
-.service('giphyService', function(fb, $firebaseArray, $firebaseObject, $q){
+// angular.module('giphTionary').service('giphyService', function($http) {
+// 	this.getGiphy = function() {
+// 		return $http({
+// 			method: 'GET',
+// 			url: 'http://api.giphy.com/'
+// 		});
+// 	};
+//   this.getGiphData = function(giphId) {
+// 		return $http({
+// 			method: 'GET',
+//       url: 'http://api.giphy.com/v1/gifs/search?q=' + giphId + '&api_key=dc6zaTOxFJmzC'
+// 		});
+// 	};
+// });
 
+angular.module('giphTionary').service('giphyService', function($http, $q){
+
+  this.getData = function(giphSearch) {
+    console.log(giphSearch);
+        var dfd = $q.defer();
+          $http({
+            method: 'GET',
+            // url: 'http://api.giphy.com/v1/gifs/search?q=' + giphSearch + '&api_key=dc6zaTOxFJmzC&limit=1',
+            url: 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + giphSearch,
+        }).then(function(response) {
+            console.log(response.data);
+            var giphResponse = response.data.data;
+            dfd.resolve(giphResponse);
+        });
+        return dfd.promise;
+    };
 
 });
